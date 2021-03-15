d8697057e5337578f145f68f0bac9677d993d0ae
shraddhapophale <52667060+shraddhapophale@users.noreply.github.com>
2021-03-15 16:26:01 +0530

    Update file-encryptor2.sh

--- file-encryptor2.sh
+++ file-encryptor2.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-file=$1
+file=$3
 
-gpg --pinentry-mode=loopback --passphrase  "root" -o /opt/cortex+encrypter/Wazuh-file-encryptor/encrypted_files/$file.gpg -c $file
+gpg --pinentry-mode=loopback --passphrase  "root" -o $file.gpg -c $file
+mv $file.gpg /opt/encrypted_files
 
 rm $file
+
+ACTION=$1
+USER=$2
+FILE=$3
+PWD=`pwd`
+
+LOCAL=`dirname $0`;
+cd $LOCAL
+cd ../
+filename=$(basename "$0")
+LOG_FILE="${PWD}/../logs/active-responses.log"
+
+echo "`date` $0 $1 $2 $3 $4 $5" >> ${LOG_FILE}
